dd54dcb306612f87f97e56efb820bd240842e440
Teayone <1968951189@qq.com>
2022-08-12 11:17:03 +0800

    update

--- src/network/request.js
+++ src/network/request.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "http://43.142.167.83:80"
+    : "http://127.0.0.1:3000";
 export function request(config) {
   const instance = axios.create({
-    baseURL: "http://127.0.0.1:3000",
+    baseURL,
     timeout: 5000,
   });
   return instance(config);
