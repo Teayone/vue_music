203d65138fad2ed4c482f29386e44aea45ab75cf
Teayone <1968951189@qq.com>
2022-08-18 13:36:14 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,13 @@
-const path = require('path')
+const path = require("path");
 module.exports = {
-  configureWebpack:(config)=>{
-    config.resolve = {
-      extensions:['.js','.json','.vue'],
-      alias:{
-        '@':path.resolve(__dirname,'./src')
-      }
-    }
-  }
-}
+  configureWebpack: (config) => {
+    (config.devtool =
+      process.env.NODE_ENV === "development" ? "source-map" : undefined),
+      (config.resolve = {
+        extensions: [".js", ".json", ".vue"],
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        },
+      });
+  },
+};
